6b31c1488dc2a1ee010438e079dac17139db6062
Alvaro Andres Perez Sanchez <72309752+iGrayFox13@users.noreply.github.com>
2021-01-26 12:47:06 -0500

    45 46

--- src/components/curso2P45.vue
+++ src/components/curso2P45.vue
@@ -1,6 +1,25 @@
 <template>
   <b-container fluid>
     <b-row>
+      <b-col></b-col>
+      <b-col sm="auto">
+        <h3 class="titulo">MODIFIER SHALL</h3>
+      </b-col>
+      <b-col></b-col>
+    </b-row>
+
+    <b-row class="diamondContainer">
+      <b-col sm="auto"><i class="fa fa-diamond fuenteDiamond"></i></b-col>
+      <b-col>
+        <h3 class="fuenteDiamond">
+          Este auxiliar es igual que “Will” pero menos común y usualmente se usa
+          con los sujetos “I y We”. además, se usa como auxiliar para pedir
+          permiso.
+        </h3>
+      </b-col>
+    </b-row>
+
+    <b-row>
       <b-col sm="auto">
         <h3 class="subTitulo">PEDIR CONSENTIMIENTO</h3>
       </b-col>
@@ -12,6 +31,26 @@
         <tablaTC :cuestionario="tabla1P45" />
       </b-col>
     </b-row>
+
+    <b-row>
+      <b-col sm="auto">
+        <h3 class="subTitulo">ORACIONES EN FUTURO</h3>
+      </b-col>
+    </b-row>
+
+    <b-col sm="1"></b-col>
+    <b-col sm="auto">
+      <p class="parrafo">Es igual que WILL, pero es menos común.</p>
+    </b-col>
+
+    <b-col sm="1"></b-col>
+    <b-col sm="auto">
+      <p class="parrafo">° I shall buy a new car (Compraré un nuevo carro) </p>
+      <p class="parrafo">° We shall not go anywhere (No iremos a ningún lado) </p>
+      <p class="parrafo">° Shall we go to New York? (¿Iremos a New York?) (¿Vamos a New York?)</p>
+    </b-col>
+
+    <b-row> </b-row>
   </b-container>
 </template>
 
@@ -25,11 +64,23 @@ export default {
     return {
       tabla1P45: {
         fields: [
-          { key: "null", label: "verbo", thClass: "noHead" },
-          { key: "html1", label: "pruebita :3", thClass: "noHead" },
+          { key: "ESPAÑOL", label: "ESPAÑOL" },
+          { key: "html1", label: "AUXILIAR" },
         ],
         items: [
-            {null:"¿Bailamos?",html1:"<span>Shall</span> we dance?"}
+          {
+            ESPAÑOL: "¿Bailamos?",
+            html1: "<span class='colorLetraRojo'>Shall</span> we dance?",
+          },
+          {
+            ESPAÑOL: "¿me voy?",
+            html1: "<span class='colorLetraRojo'>Shall</span> I go?",
+          },
+          {
+            ESPAÑOL: "¿empezamos la clase?",
+            html1:
+              "<span class='colorLetraRojo'>Shall</span> we begin the class?",
+          },
         ],
       },
     };
---
+++ src/components/curso2P46.vue
@@ -0,0 +1,52 @@
+<template>
+  <b-container fluid>
+      <b-row>
+      <b-col></b-col>
+      <b-col sm="auto">
+        <h3 class="titulo">MODIFIER WILL</h3>
+      </b-col>
+      <b-col></b-col>
+    </b-row>
+
+    <b-row class="diamondContainer">
+      <b-col sm="auto"><i class="fa fa-diamond fuenteDiamond"></i></b-col>
+      <b-col>
+        <h3 class="fuenteDiamond">Este es el auxiliar del futuro.</h3>
+         <h3 class="fuenteDiamond"><strong>- Este es el auxiliar del futuro.</strong></h3>
+        <h3 class="parrafo">° Will you sit down, please?  (¿Podría usted sentarse, por favor? O ¿Quiere usted sentarse por favor?.</h3>
+        <h3 class="parrafo">° Will you close the door, please?</h3>
+        <h3 class="parrafo">° Will you help me carry these bags? </h3>
+       </b-col>
+    </b-row>  
+    <b-row class="diamondContainer">
+      <b-col sm="auto"><i class="fa fa-diamond fuenteDiamond"></i></b-col>
+      <b-col>
+         <h3 class="fuenteDiamond"><strong>- También significa SOLER, es decir tener el hábito de.</strong></h3>
+        <h3 class="parrafo">° He will always tell you about his experiences in the army even if you are not interested in it.</h3>
+        <h3 class="parrafo">° I will sit on a chair thinking about my plans</h3>
+        <h3 class="parrafo">° She will talk about her trips for hours on en </h3>
+        </b-col>
+    </b-row>
+    <b-row class="diamondContainer">
+      <b-col sm="auto"><i class="fa fa-diamond fuenteDiamond"></i></b-col>
+      <b-col>
+        <h3 class="fuenteDiamond"><strong>- Además, significa testamento, voluntad o albedrío.</strong></h3>
+        <h3 class="parrafo">° Her father left her the house in his will. (Su padre le dejó la casa en su testamento)</h3>
+        <h3 class="parrafo">° Force of Will: fuerza de voluntad </h3>
+        <h3 class="parrafo">° Free will: libre albedrío  </h3>
+
+      </b-col>
+    </b-row>
+
+  </b-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
