7b79dc583a8cde87a99f3e1edebcffe25a640d15
Alvaro Andres Perez Sanchez <72309752+iGrayFox13@users.noreply.github.com>
2021-01-27 22:16:43 -0500

    50,51 terminadas

---
+++ src/components/curso2P50.vue
@@ -0,0 +1,159 @@
+<template>
+  <b-container fluid>
+    <b-row>
+      <b-col></b-col>
+      <b-col sm="auto">
+        <h3 class="titulo">OUGHT TO</h3>
+      </b-col>
+      <b-col></b-col>
+    </b-row>
+
+    <b-row>
+      <b-col sm="1"></b-col>
+      <b-col sm="auto">
+        <p class="parrafo">
+          <span class="colorLetraRojo">Negación:</span> OUGHT NOT
+        </p>
+        <p class="parrafo">
+          <span class="colorLetraRojo">Significado:</span> Debería
+        </p>
+        <p class="parrafo"><span class="colorLetraRojo">Uso:</span></p>
+        <p class="parrafo">
+          1.Es igual que <span class="colorLetraRojo">SHOULD</span>, pero menos
+          común.
+        </p>
+        <b-row>
+          <b-col sm="1"></b-col>
+          <b-col sm="auto">
+            <p class="parrafo">
+              Example:- We
+              <span class="colorLetraRojo"> ought to </span>eat less
+            </p>
+          </b-col>
+        </b-row>
+        <p class="parrafo">
+          2. Se usa para hablar de obligaciones y recomendaciones.
+        </p>
+        <b-row>
+          <b-col sm="1"></b-col>
+          <b-col sm="auto">
+            <p class="parrafo">
+              Example: - You
+              <span class="colorLetraRojo"> ought to </span> finish your
+              homework before you go out to play.
+            </p>
+          </b-col>
+        </b-row>
+        <p class="parrafo">
+          3. No se usa con el sujeto “I” cuando se trata de un consejo y en
+          lugar se usa “should”.
+        </p>
+        <b-row>
+          <b-col sm="1"></b-col>
+          <b-col sm="auto">
+            <p class="parrafo">
+              Example: - I
+              <span class="colorLetraRojo">should</span> travel by bus rather
+              than by plane
+            </p>
+          </b-col>
+        </b-row>
+
+        <p class="parrafo">
+          4.Para hablar de una crítica o algo que no pasó en el pasado y
+          lamentamos que no.
+        </p>
+        <b-row>
+          <b-col sm="1"></b-col>
+          <b-col sm="auto">
+            <p class="parrafo">
+              Example: -She
+              <span class="colorLetraRojo"> ought to </span> have arrived in
+              Moscow early this morning if the plane
+            </p>
+
+            <p class="parrafo">was on time.</p>
+          </b-col>
+        </b-row>
+      </b-col>
+    </b-row>
+
+    <b-row>
+      <b-col sm="auto">
+        <h3 class="subTitulo">
+          PRACTICE. Fill up the gaps with Ought to or ought
+        </h3>
+      </b-col>
+    </b-row>
+
+    <b-row>
+      <b-col sm="1"></b-col>
+      <b-col sm="11">
+        <b-row
+          v-for="(item, index) in listadoCompletarP50"
+          :key="index + item.textoA"
+          class="margin-bottom"
+        >
+          <b-col
+            ><inputChecked
+              :esperado="item.listado"
+              :conTexto="true"
+              :textoA="item.textoA"
+              :textoD="item.textoD"
+              :tamano="item.tamano"
+              :textoD2="item.textoD2"
+          /></b-col>
+        </b-row>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import inputChecked from "./inputChecked";
+export default {
+  components: {
+    inputChecked,
+  },
+  data() {
+    return {
+      listadoCompletarP50: [
+          {
+          listado: ["should"],
+          textoA: "1. Life is too short and time is precious therefore we",
+          textoD: "live it intensively.",
+          tamano: "58.5",
+        },
+        {
+          listado: ["should"],
+          textoA: "2. People ",
+          textoD: "not drive a car without a license. They may be fined.",
+          tamano: "53.8",
+        },
+        {
+          listado: ["should"],
+          textoA: "3. It is very dangerous to walk along the park late at night, parents ",
+          textoD: "not  let their",
+          textoD2:"children go out alone",
+          tamano: "64",
+        },
+        {
+          listado: ["should"],
+          textoA: "4. You",
+          textoD: " not speak with your mouth full.",
+          tamano: "37.3",
+        },
+        {
+          listado: ["should"],
+          textoA: "5. Think about what I told you! I insist. You",
+          textoD: "not call her anymore. ",
+          tamano: "55",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/curso2P51.vue
@@ -0,0 +1,147 @@
+<template>
+  <b-container fluid>
+    <b-row>
+      <b-col></b-col>
+      <b-col sm="auto">
+        <h3 class="titulo">HAD BETTER</h3>
+      </b-col>
+      <b-col></b-col>
+    </b-row>
+    <b-row>
+      <b-col sm="1"></b-col>
+      <b-col sm="auto">
+        <p class="parrafo">
+          <span class="colorLetraRojo">Contracción y negación: </span>'d better,
+          had better not
+        </p>
+        <p class="parrafo">
+          <span class="colorLetraRojo">Significado:</span> Cuando creemos que
+          algo es una buena idea de hacer algo.
+        </p>
+        <p class="parrafo">
+          Es más usado en el inglés hablado y es igual que SHOULD.
+        </p>
+        <p class="parrafo"><span class="colorLetraRojo">Uso:</span></p>
+        <b-row>
+          <b-col sm="1"></b-col>
+          <b-col sm="auto">
+            <p class="parrafo">Example:</p>
+            <b-row>
+              <b-col sm="1"></b-col>
+              <b-col sm="auto">
+                <p class="parrafo">
+                  ° You had better stop drinking. (Es mejor que dejes de tomar)
+                </p>
+                <p class="parrafo">
+                  ° We had better take a taxi back home. (Es mejor que tomemos</p>
+                 <p class="parrafo">   un taxi de regreso a casa)</p>
+                 <p class="parrafo">
+                  ° He had better save his money. (Él debería ahorrar su dinero) 
+                </p>
+                <p class="parrafo">
+                  ° You had better tell the truth. (Es mejor si dices la verdad) 
+                </p>
+                <p class="parrafo">
+                  ° It's raining, you had better not go out. (Está lloviendo, mejor no salgas)
+                </p>
+                <p class="parrafo">
+                  ° You'd better not be late for work next time.</p> <p class="parrafo">(Más te vale que no llegues tarde al trabajo la próxima vez
+                </p>
+              </b-col>
+            </b-row>
+          </b-col>
+        </b-row>
+      </b-col>
+    </b-row>
+
+
+    <b-row>
+      <b-col sm="auto">
+        <h3 class="subTitulo">
+          PRACTICE. Complete the gaps with had better or had better not 
+        </h3>
+      </b-col>
+    </b-row>
+
+    <b-row>
+      <b-col sm="1"></b-col>
+      <b-col sm="11">
+        <b-row
+          v-for="(item, index) in listadoCompletarP51"
+          :key="index + item.textoA"
+          class="margin-bottom"
+        >
+          <b-col
+            ><inputChecked
+              :esperado="item.listado"
+              :conTexto="true"
+              :textoA="item.textoA"
+              :textoD="item.textoD"
+              :tamano="item.tamano"
+              :textoD2="item.textoD2"
+          /></b-col>
+        </b-row>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import inputChecked from "./inputChecked";
+export default {
+  components: {
+    inputChecked,
+  },
+  data() {
+    return {
+      listadoCompletarP51: [
+          {
+          listado: ["had better"],
+          textoA: "1. If you keep drinking beer, you will get drunk so you",
+          textoD: "go home now. ",
+          tamano: "59.8",
+        },
+        {
+          listado: ["had better"],
+          textoA: "2. You",
+          textoD: " write it down on a piece of paper.",
+          tamano: "41.3",
+        },
+        {
+          listado: ["had better"],
+          textoA: "3. You are very sick! – you ",
+          textoD: " not go to work today.",
+          tamano: "46.3",
+        },
+        {
+          listado: ["had better"],
+          textoA: "4.  The party starts at 7:00 pm. You ",
+          textoD: "be there in time to make sure everything is alright. ",
+          tamano: "71",
+        },
+        {
+          listado: ["had better"],
+          textoA: "5.You",
+          textoD: "stay home or you will get infected. ",
+          tamano: "41.4",
+        },
+        {
+          listado: ["had better"],
+          textoA: "6. We",
+          textoD: "hurry up or we will miss the bus. ",
+          tamano: "40.3",
+        },
+        {
+          listado: ["had better"],
+          textoA: "7.  It is getting late and buses don’t run this time so I ",
+          textoD: "go home now. ",
+          tamano: "59",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
